12ee059548a416b62577565eed23d55a0ca7a0dc
David R Saeva <11340638+drsaeva@users.noreply.github.com>
2024-05-20 00:36:56 -0400

    more comments

--- app/clickable-canvas/travel-calculator.ts
+++ app/clickable-canvas/travel-calculator.ts
@@ -8,7 +8,66 @@ import {
 } from '../common/types'
 import { defaultTransportation } from '../common/constants'
 
-export default (
+/**
+ * Calculates the linear distance in miles on the map between the two points
+ * using the pythagorean theorem.
+ * 
+ * @param start starting coordinate on the cartesian plane 
+ * @param end end coordinate on the cartesian plane
+ * @param scale the amount of pixels per mile on the map
+ * @returns the amount of miles between the two points
+ */
+const calculateDistance = (
+  start: LocationProps,
+  end: LocationProps,
+  scale: number
+): number => {
+  const delta: Vector2d = { x: end.x - start.x, y: end.y - start.y }
+  const distance: number = Math.sqrt(
+    Math.pow( delta.x, 2 ) + Math.pow( delta.y, 2 )
+  )
+
+  return Math.floor( distance * scale )
+}
+
+/**
+ * Generates a string describing the amount of time in hours and days it takes
+ * in the game world to travel between the two points using a given mode of
+ * transportation.
+ * 
+ * @param mode mode of transportation, including a rate (mph), the maximum time
+ *             per day the mode can travel, and a description (name)
+ * @param distance the distance the given mode of transportation is covering
+ * @returns a string describing how long the mode of transportation will take
+ */
+const calculateTravel = (
+  mode: TransportationMode,
+  distance: number
+): string => {
+  const rawHours: number = distance / mode.rate
+  const rawDays: number = rawHours / mode.maxLength
+  const days: number = Math.floor( rawDays )
+  const hours: number = Math.round( ( rawDays - days ) * mode.maxLength )
+  const hourText: string = hours + ' hour' + ( hours === 1 ? '' : 's' )
+  const dur: string =
+    days >= 1
+      ? days + ' day' + ( days === 1 ? '' : 's' ) + ', ' + hourText
+      : hourText
+
+  return '\n' + mode.desc + ': ' + dur
+}
+
+/**
+ * Generates a string describing the distance being traveled, in miles, and how
+ * long several different modes of transportation in-game will take to cover the
+ * distance in hours and days.
+ * 
+ * @param start starting point cartesian coordinates and whether to display it 
+ * @param end ending point cartesian coordinates and whether to display it
+ * @param view characteristics of the map, including its dimensions and scale
+ * @returns the description string for the travel
+ */
+const TravelCalculator = (
   start: LocationProps,
   end: LocationProps,
   view: MapView
@@ -30,32 +89,4 @@ export default (
   return travelText
 }
 
-const calculateDistance = (
-  start: LocationProps,
-  end: LocationProps,
-  scale: number
-): number => {
-  const delta: Vector2d = { x: end.x - start.x, y: end.y - start.y }
-  const distance: number = Math.sqrt(
-    Math.pow( delta.x, 2 ) + Math.pow( delta.y, 2 )
-  )
-
-  return Math.floor( distance * scale )
-}
-
-const calculateTravel = (
-  mode: TransportationMode,
-  distance: number
-): string => {
-  const rawHours: number = distance / mode.rate
-  const rawDays: number = rawHours / mode.maxLength
-  const days: number = Math.floor( rawDays )
-  const hours: number = Math.round( ( rawDays - days ) * mode.maxLength )
-  const hourText: string = hours + ' hour' + ( hours === 1 ? '' : 's' )
-  const dur: string =
-    days >= 1
-      ? days + ' day' + ( days === 1 ? '' : 's' ) + ', ' + hourText
-      : hourText
-
-  return '\n' + mode.desc + ': ' + dur
-}
+export default TravelCalculator
